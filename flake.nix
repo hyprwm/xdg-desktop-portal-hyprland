64c0968e4a3bf1f371c720f3bfc57c41e69e5461
Jacob Birkett <spikespaz@outlook.com>
2023-05-01 20:10:10 -0700

    flake: fix usage of prev in overlays
    flake: use overlays in pkgsFor

--- flake.nix
+++ flake.nix
@@ -20,7 +20,14 @@
       "aarch64-linux"
       "x86_64-linux"
     ];
-    pkgsFor = nixpkgs.legacyPackages;
+    pkgsFor = genSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.default
+          inputs.hyprland-protocols.overlays.default
+        ];
+      });
     mkDate = longDate: (lib.concatStringsSep "-" [
       (builtins.substring 0 4 longDate)
       (builtins.substring 4 2 longDate)
@@ -28,18 +35,18 @@
     ]);
     version = "0.pre" + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
   in {
-    overlays.default = _: prev: rec {
-      xdg-desktop-portal-hyprland = prev.callPackage ./nix/default.nix {
-        stdenv = prev.gcc12Stdenv;
-        inherit (inputs.hyprland-protocols.packages.${prev.hostPlatform.system}) hyprland-protocols;
-        inherit hyprland-share-picker version;
+    overlays.default = final: prev: {
+      xdg-desktop-portal-hyprland = final.callPackage ./nix/default.nix {
+        stdenv = final.gcc12Stdenv;
+        inherit (final) hyprland-protocols hyprland-share-picker;
+        inherit version;
       };
 
-      hyprland-share-picker = prev.libsForQt5.callPackage ./nix/hyprland-share-picker.nix {inherit version;};
+      hyprland-share-picker = final.libsForQt5.callPackage ./nix/hyprland-share-picker.nix {inherit version;};
     };
 
     packages = genSystems (system:
-      (self.overlays.default null pkgsFor.${system})
+      (self.overlays.default pkgsFor.${system} pkgsFor.${system})
       // {default = self.packages.${system}.xdg-desktop-portal-hyprland;});
 
     formatter = genSystems (system: pkgsFor.${system}.alejandra);
