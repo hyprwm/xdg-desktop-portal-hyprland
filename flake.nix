c5b241dc0ce36169168246ced0426a44d95f03a3
Jacob Birkett <spikespaz@outlook.com>
2023-08-09 12:52:35 -0700

    flake: make systems overrideable

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # <https://github.com/nix-systems/nix-systems>
+    systems.url = "github:nix-systems/default-linux";
+
     hyprland-protocols = {
       url = "github:hyprwm/hyprland-protocols";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,16 +16,14 @@
   outputs = {
     self,
     nixpkgs,
+    systems,
     ...
   } @ inputs: let
     inherit (nixpkgs) lib;
-    genSystems = lib.genAttrs [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-    pkgsFor = genSystems (system:
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system:
       import nixpkgs {
-        inherit system;
+        localSystem = system;
         overlays = [
           self.overlays.default
           inputs.hyprland-protocols.overlays.default
@@ -31,10 +32,10 @@
   in {
     overlays = import ./nix/overlays.nix {inherit self inputs lib;};
 
-    packages = genSystems (system:
+    packages = eachSystem (system:
       (self.overlays.default pkgsFor.${system} pkgsFor.${system})
       // {default = self.packages.${system}.xdg-desktop-portal-hyprland;});
 
-    formatter = genSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
   };
 }
