8344221daa2f09b09d8d7d230d4cb4cb38148692
Oliver Enes <oliver@enesnet.de>
2023-11-04 23:17:10 +0100

    added inShellPath and sendEmptyDbusMethodReply helper

--- src/helpers/MiscFunctions.hpp
+++ src/helpers/MiscFunctions.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
+#include <sdbus-c++/Message.h>
 
 std::string execAndGet(const char* cmd);
 void addHyprlandNotification(const std::string& icon, float timeMs, const std::string& color, const std::string& message);
-bool fileExists(std::string path);
+bool inShellPath(const std::string& exec);
+void sendEmptyDbusMethodReply(sdbus::MethodCall& call, u_int32_t responseCode);
