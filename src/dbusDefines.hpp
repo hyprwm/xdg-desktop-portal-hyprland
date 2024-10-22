fa8598ed089244bef16929a4972cd0e8f2d89d9b
Vaxry <vaxry@vaxry.net>
2024-10-22 02:10:44 +0100

    oopsie LMAO

--- src/dbusDefines.hpp
+++ src/dbusDefines.hpp
@@ -2,4 +2,4 @@
 
 #include <sdbus-c++/sdbus-c++.h>
 
-typedef sdbus::Struct<uint32_t, std::unordered_map<std::string, sdbus::Variant>> dbUasv;
+typedef std::tuple<uint32_t, std::unordered_map<std::string, sdbus::Variant>> dbUasv;
