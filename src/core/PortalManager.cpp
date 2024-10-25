f66d690c571d65d7d3e1c07952459345131b3e51
Gwilherm Folliot <gwilherm55fo@gmail.com>
2024-10-26 01:33:06 +0200

    input-capture: update to sdbus2.0

--- src/core/PortalManager.cpp
+++ src/core/PortalManager.cpp
@@ -388,7 +388,7 @@ void CPortalManager::startEventLoop() {
         m_mEventLock.lock();
 
         if (m_sEventLoopInternals.pollFds[0].revents & POLLIN /* dbus */) {
-            while (m_pConnection->processPendingRequest()) {
+            while (m_pConnection->processPendingEvent()) {
                 ;
             }
         }
