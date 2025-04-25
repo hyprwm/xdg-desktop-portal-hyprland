e41015bebb8ddb175659ddb18469e39fd103b517
WhySoBad <49595640+WhySoBad@users.noreply.github.com>
2025-04-25 11:29:08 +0200

    chore: implemented feedback

--- src/shared/ToplevelMappingManager.cpp
+++ src/shared/ToplevelMappingManager.cpp
@@ -1,6 +1,5 @@
 #include "ToplevelMappingManager.hpp"
 #include "../helpers/Log.hpp"
-#include "../core/PortalManager.hpp"
 
 CToplevelMappingManager::CToplevelMappingManager(SP<CCHyprlandToplevelMappingManagerV1> mgr) : m_pManager(mgr) {
     Debug::log(LOG, "[toplevel mapping] registered manager");
@@ -15,16 +14,16 @@ void CToplevelMappingManager::fetchWindowForToplevel(SP<CCZwlrForeignToplevelHan
 
     m_vHandles.push_back(HANDLE);
 
-    HANDLE->setWindowAddress([handle](CCHyprlandToplevelWindowMappingHandleV1* h, uint32_t address_hi, uint32_t address) {
+    HANDLE->setWindowAddress([this, handle](CCHyprlandToplevelWindowMappingHandleV1* h, uint32_t address_hi, uint32_t address) {
         const auto ADDRESS = (uint64_t)address_hi << 32 | address;
-        g_pPortalManager->m_sHelpers.toplevelMapping->m_muAddresses.insert_or_assign(handle, ADDRESS);
+        m_muAddresses.insert_or_assign(handle, ADDRESS);
         Debug::log(TRACE, "[toplevel mapping] mapped toplevel at {} to window {}", (void*)handle.get(), ADDRESS);
-        std::erase_if(g_pPortalManager->m_sHelpers.toplevelMapping->m_vHandles, [&](const auto& other) { return other.get() == h; });
+        std::erase_if(m_vHandles, [&](const auto& other) { return other.get() == h; });
     });
 
-    HANDLE->setFailed([handle](CCHyprlandToplevelWindowMappingHandleV1* h) {
+    HANDLE->setFailed([this, handle](CCHyprlandToplevelWindowMappingHandleV1* h) {
         Debug::log(TRACE, "[toplevel mapping] failed to map toplevel at {} to window", (void*)handle.get());
-        std::erase_if(g_pPortalManager->m_sHelpers.toplevelMapping->m_vHandles, [&](const auto& other) { return other.get() == h; });
+        std::erase_if(m_vHandles, [&](const auto& other) { return other.get() == h; });
     });
 }
 
