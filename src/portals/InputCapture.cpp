436619a06641d7090e36416546d26b9aee2f0679
Gwilherm Folliot <gwilherm55fo@gmail.com>
2025-08-21 23:49:18 +0200

    inputcapture: cleanup barrier intersection method

--- src/portals/InputCapture.cpp
+++ src/portals/InputCapture.cpp
@@ -4,7 +4,6 @@
 #include "../helpers/Log.hpp"
 #include "../shared/Session.hpp"
 #include "hyprland-input-capture-v1.hpp"
-#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <sdbus-c++/Types.h>
@@ -339,31 +338,19 @@ void CInputCapturePortal::onForceRelease() {
         disable(value->sessionHandle);
 }
 
-bool get_line_intersection(double p0_x, double p0_y, double p1_x, double p1_y, double p2_x, double p2_y, double p3_x, double p3_y, double* i_x, double* i_y) {
-    float s1_x, s1_y, s2_x, s2_y;
-    s1_x = p1_x - p0_x;
-    s1_y = p1_y - p0_y;
-    s2_x = p3_x - p2_x;
-    s2_y = p3_y - p2_y;
+bool isLineIntersect(double p0X, double p0Y, double p1X, double p1Y, double p2X, double p2Y, double p3X, double p3Y) {
+    float s1X = p1X - p0X;
+    float s1Y = p1Y - p0Y;
+    float s2X = p3X - p2X;
+    float s2Y = p3Y - p2Y;
+    float s = (-s1Y * (p0X - p2X) + s1X * (p0Y - p2Y)) / (-s2X * s1Y + s1X * s2Y);
+    float t = (s2X * (p0Y - p2Y) - s2Y * (p0X - p2X)) / (-s2X * s1Y + s1X * s2Y);
 
-    float s, t;
-    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-    t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
-        // Collision detected
-        if (i_x != NULL)
-            *i_x = p0_x + (t * s1_x);
-        if (i_y != NULL)
-            *i_y = p0_y + (t * s1_y);
-        return 1;
-    }
-
-    return 0; // No collision
+    return s >= 0 && s <= 1 && t >= 0 && t <= 1;
 }
 
 bool testCollision(SBarrier barrier, double px, double py, double nx, double ny) {
-    return get_line_intersection(barrier.x1, barrier.y1, barrier.x2, barrier.y2, px, py, nx, ny, nullptr, nullptr);
+    return isLineIntersect(barrier.x1, barrier.y1, barrier.x2, barrier.y2, px, py, nx, ny);
 }
 
 uint32_t CInputCapturePortal::SSession::isColliding(double px, double py, double nx, double ny) {
