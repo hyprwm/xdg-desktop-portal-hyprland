a38ba77301bfc356c60fcd19829a60c544c5dcc6
thkinh <139554989+thkinh@users.noreply.github.com.>
2026-05-22 12:26:43 +0700

    first commit on branch hyprtoolkit-picker

--- hyprland-share-picker/main.cpp
+++ hyprland-share-picker/main.cpp
@@ -1,44 +1,90 @@
-#include <QApplication>
-#include <QEvent>
-#include <QObject>
-#include <QPushButton>
-#include <QScreen>
-#include <QTabWidget>
-#include <QWidget>
-#include <QtDebug>
-#include <QtWidgets>
-#include <QSettings>
-#include <array>
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
+#include "Element.hpp"
+#include <cstdint>
+#include <functional>
+#include <hyprtoolkit/types/FontTypes.hpp>
+#include <hyprtoolkit/types/SizeType.hpp>
+#include <pixman-1/pixman.h>
+#include <hyprtoolkit/core/CoreMacros.hpp>
+#include <hyprtoolkit/window/Window.hpp>
+#include <hyprtoolkit/core/Backend.hpp>
+#include <hyprutils/math/Vector2D.hpp>
+#include <hyprtoolkit/element/ColumnLayout.hpp>
+#include <hyprtoolkit/element/Rectangle.hpp>
+#include <hyprtoolkit/element/RowLayout.hpp>
+#include <hyprtoolkit/element/ScrollArea.hpp>
+#include <hyprtoolkit/element/Button.hpp>
+#include <hyprtoolkit/element/Image.hpp>
+#include <hyprtoolkit/element/Text.hpp>
+#include <hyprtoolkit/element/Null.hpp>
 #include <string>
-#include <vector>
-#include <hyprutils/os/Process.hpp>
-using namespace Hyprutils::OS;
 
-#include "mainpicker.h"
-#include "elidedbutton.h"
 
-std::string execAndGet(const char* cmd) {
-    std::string command = cmd + std::string{" 2>&1"};
-    CProcess    proc("/bin/sh", {"-c", cmd});
+using namespace Hyprutils::Memory;
+using namespace Hyprutils::Math;
+using namespace Hyprtoolkit;
 
-    if (!proc.runSync())
-        return "error";
+#define SP CSharedPointer
+#define WP CWeakPointer
+#define UP CUniquePointer
 
-    return proc.stdOut();
-}
+static SP<IBackend> backend;
 
-QApplication* pickerPtr     = nullptr;
-MainPicker*   mainPickerPtr = nullptr;
+static const std::vector<std::string> sourceGroups = {
+    "Monitor", 
+    "Window", 
+    "Workspace", 
+    "Region"
+};
 
 struct SWindowEntry {
     std::string        name;
     std::string        clazz;
     unsigned long long id = 0;
 };
+
+struct SWorkspaceEntry {
+    std::string name;
+    int64_t id;
+};
+
+struct SMonitorEntry {
+    std::string name;
+    int64_t width;
+    int64_t height;
+};
+
+struct SRegion {
+    int64_t x;
+    int64_t y;
+    int64_t size_x;
+    int64_t size_y;
+};
+
+std::vector<SWorkspaceEntry> getWorkspaces(const char* env) {
+    std::vector<SWorkspaceEntry> result;
+
+    if (!env)
+        return result;
+
+    std::string rolling = env;
+    while(!rolling.empty()) {
+        const auto IDSEPPOS = rolling.find("[WI>]");
+        const auto IDSTR    = rolling.substr(0, IDSEPPOS);
+
+        const auto NAMESEPPOS = rolling.find("[WN>]");
+        const auto NAMESTR    = rolling.substr(IDSEPPOS + 5, NAMESEPPOS - IDSEPPOS - 5);
+
+        try {
+            result.push_back({NAMESTR, std::stoll(IDSTR)});
+        } catch (std::exception& e) {
+            // silent err
+        }
+
+        rolling = rolling.substr(NAMESEPPOS + 5);
+    }
+
+    return result;
+}
 
 std::vector<SWindowEntry> getWindows(const char* env) {
     std::vector<SWindowEntry> result;
@@ -77,185 +123,102 @@ std::vector<SWindowEntry> getWindows(const char* env) {
     return result;
 }
 
-int main(int argc, char* argv[]) {
-    qputenv("QT_LOGGING_RULES", "qml=false");
 
-    bool allowTokenByDefault = false;
-    for (int i = 1; i < argc; ++i) {
-        if (argv[i] == std::string{"--allow-token"})
-            allowTokenByDefault = true;
+SP<IWindow> windowLayout() {
+    auto window = CWindowBuilder::begin() ->preferredSize({720, 650})
+        ->minSize({650, 600})
+        ->appTitle("Select what to share")
+        ->appClass("hyprland-share-picker")
+        ->commence();
+
+    auto rect1 = CRectangleBuilder::begin()
+        ->color([] { return CHyprColor{0x547A95FF}; })
+        ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_PERCENT, Vector2D{1.f, 0.08f} })
+        ->rounding(5)
+        ->commence();
+
+    auto rect2 = CRectangleBuilder::begin()
+        ->color([] { return CHyprColor{0x547A95FF}; })
+        ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_PERCENT, Vector2D{1.f, 0.5f} })
+        ->rounding(5)
+        ->commence();
+    rect2->setGrow(true);
+
+    // background
+    window->m_rootElement->addChild( CRectangleBuilder::begin() ->color([] { return CHyprColor{0xE8EDF2FF}; }) ->commence());
+
+    // main layout
+    auto mainLayout = CColumnLayoutBuilder::begin()
+        ->gap(5)
+        ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_PERCENT, Vector2D{1.f, 1.f} })
+        ->commence();
+    mainLayout->setPositionMode(IElement::HT_POSITION_ABSOLUTE);
+    mainLayout->setPositionFlag(IElement::HT_POSITION_FLAG_VCENTER, true);
+
+    auto tabButtons = CRowLayoutBuilder::begin()
+        ->gap(5)
+        ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_PERCENT, Vector2D{1.f, 1.f} })
+        ->commence();
+
+    tabButtons->setPositionMode(IElement::HT_POSITION_ABSOLUTE);
+    tabButtons->setPositionFlag(IElement::HT_POSITION_FLAG_VCENTER, true);
+    tabButtons->setMargin(5.f);
+
+    for (auto& sourceGroup : sourceGroups) {
+        auto tabButton = CButtonBuilder::begin()
+            ->label(std::string(sourceGroup))
+            ->onMainClick([&sourceGroup](SP<CButtonElement> el){ el->rebuild() ->label(std::format("Clicked {}", sourceGroup)) ->commence(); })
+            ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_PERCENT, {0.18f, 0.85f} })
+            ->fontSize(CFontSize::HT_FONT_SMALL)
+            ->commence();
+        tabButtons->addChild(tabButton);
     }
 
-    const char*  WINDOWLISTSTR = getenv("XDPH_WINDOW_SHARING_LIST");
-    const auto   WINDOWLIST    = getWindows(WINDOWLISTSTR);
+    // Sources layout
+    auto scroll = CScrollAreaBuilder::begin()->scrollY(true)->size({CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_PERCENT, {1.f, 1.f}})->commence();
+    auto sourceButtons = CColumnLayoutBuilder::begin()
+        ->gap(5)
+        ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_AUTO, {1.f, 0.f} })
+        ->commence();
+    sourceButtons->setMargin(5.f);
 
-    QApplication picker(argc, argv);
-    pickerPtr = &picker;
-    MainPicker w;
-    mainPickerPtr = &w;
-
-    QSettings* settings = new QSettings("/tmp/hypr/hyprland-share-picker.conf", QSettings::IniFormat);
-    w.setGeometry(0, 0, settings->value("width").toInt(), settings->value("height").toInt());
-
-    QCoreApplication::setApplicationName("org.hyprland.xdg-desktop-portal-hyprland");
-
-    // get the tabwidget
-    const auto TABWIDGET        = w.findChild<QTabWidget*>("tabWidget");
-    const auto ALLOWTOKENBUTTON = w.findChild<QCheckBox*>("checkBox");
-
-    if (allowTokenByDefault)
-        ALLOWTOKENBUTTON->setCheckState(Qt::CheckState::Checked);
-
-    const auto TAB1 = (QWidget*)TABWIDGET->children()[0];
-
-    const auto SCREENS_SCROLL_AREA_CONTENTS =
-        (QWidget*)TAB1->findChild<QWidget*>("screens")->findChild<QScrollArea*>("scrollArea")->findChild<QWidget*>("scrollAreaWidgetContents");
-
-    const auto SCREENS_SCROLL_AREA_CONTENTS_LAYOUT = SCREENS_SCROLL_AREA_CONTENTS->layout();
-
-    // add all screens
-    const auto    SCREENS = picker.screens();
-
-    constexpr int BUTTON_HEIGHT = 41;
-
-    for (int i = 0; i < SCREENS.size(); ++i) {
-        const auto    GEOMETRY = SCREENS[i]->geometry();
-
-        QString       text = QString::fromStdString(std::string("Screen " + std::to_string(i) + " at " + std::to_string(GEOMETRY.x()) + ", " + std::to_string(GEOMETRY.y()) + " (" +
-                                                                std::to_string(GEOMETRY.width()) + "x" + std::to_string(GEOMETRY.height()) + ") (") +
-                                                    SCREENS[i]->name().toStdString() + ")");
-        QString       outputName = SCREENS[i]->name();
-        ElidedButton* button     = new ElidedButton(text);
-        button->setMinimumSize(0, BUTTON_HEIGHT);
-        SCREENS_SCROLL_AREA_CONTENTS_LAYOUT->addWidget(button);
-
-        QObject::connect(button, &QPushButton::clicked, [=]() {
-            std::cout << "[SELECTION]";
-            std::cout << (ALLOWTOKENBUTTON->isChecked() ? "r" : "");
-            std::cout << "/";
-
-            std::cout << "screen:" << outputName.toStdString() << "\n";
-
-            settings->setValue("width", mainPickerPtr->width());
-            settings->setValue("height", mainPickerPtr->height());
-            settings->sync();
-
-            pickerPtr->quit();
-            return 0;
-        });
+    for (auto& source : sourceGroups) {
+        auto sourceButton = CButtonBuilder::begin()
+            ->label(std::string("Source: ") + std::string(source))
+            ->onMainClick([&source](SP<CButtonElement> el) { el->rebuild() ->label(std::format("Source {}", source)) ->commence(); })
+            ->size({ CDynamicSize::HT_SIZE_PERCENT, CDynamicSize::HT_SIZE_ABSOLUTE, {0.995f, 60.f} })
+            ->alignText(Hyprtoolkit::HT_FONT_ALIGN_CENTER)
+            ->commence();
+        sourceButtons->addChild(sourceButton);
     }
 
-    QSpacerItem* SCREENS_SPACER = new QSpacerItem(0, 10000, QSizePolicy::Expanding, QSizePolicy::Expanding);
-    SCREENS_SCROLL_AREA_CONTENTS_LAYOUT->addItem(SCREENS_SPACER);
 
-    // windows
-    const auto WINDOWS_SCROLL_AREA_CONTENTS =
-        (QWidget*)TAB1->findChild<QWidget*>("windows")->findChild<QScrollArea*>("scrollArea_2")->findChild<QWidget*>("scrollAreaWidgetContents_2");
+    window->m_rootElement->addChild(mainLayout);
 
-    const auto WINDOWS_SCROLL_AREA_CONTENTS_LAYOUT = WINDOWS_SCROLL_AREA_CONTENTS->layout();
+    mainLayout->setMargin(10.f);
+    mainLayout->addChild(rect1);
+    mainLayout->addChild(rect2);
+    rect1->addChild(tabButtons);
+    rect2->addChild(scroll);
+    scroll->addChild(sourceButtons);
 
-    // loop over them
-    int windowIterator = 0;
-    for (auto& window : WINDOWLIST) {
-        QString       text = QString::fromStdString(window.clazz + ": " + window.name);
+    return window;
+}
 
-        ElidedButton* button = new ElidedButton(text);
-        button->setMinimumSize(0, BUTTON_HEIGHT);
-        WINDOWS_SCROLL_AREA_CONTENTS_LAYOUT->addWidget(button);
+static void changeTab(const std::string& sourceGroup, SP<CScrollAreaElement> tab)  {
+}
 
-        mainPickerPtr->windowIDs[button] = window.id;
+static void onSelectSource(std::string sourceString) {
+}
 
-        QObject::connect(button, &QPushButton::clicked, [=]() {
-            std::cout << "[SELECTION]";
-            std::cout << (ALLOWTOKENBUTTON->isChecked() ? "r" : "");
-            std::cout << "/";
+int main(int argc, char** argv, char** envp) {
+    backend = IBackend::create();
 
-            std::cout << "window:" << mainPickerPtr->windowIDs[button] << "\n";
+    auto window = windowLayout();
 
-            settings->setValue("width", mainPickerPtr->width());
-            settings->setValue("height", mainPickerPtr->height());
-            settings->sync();
+    window->open();
 
-            pickerPtr->quit();
-            return 0;
-        });
+    backend->enterLoop();
 
-        windowIterator++;
-    }
-
-    QSpacerItem* WINDOWS_SPACER = new QSpacerItem(0, 10000, QSizePolicy::Expanding, QSizePolicy::Expanding);
-    WINDOWS_SCROLL_AREA_CONTENTS_LAYOUT->addItem(WINDOWS_SPACER);
-
-    // lastly, region
-    const auto    REGION_OBJECT = (QWidget*)TAB1->findChild<QWidget*>("region");
-    const auto    REGION_LAYOUT = REGION_OBJECT->layout();
-
-    QString       text = "Select region...";
-
-    ElidedButton* button = new ElidedButton(text);
-    button->setMaximumSize(400, BUTTON_HEIGHT);
-    REGION_LAYOUT->addWidget(button);
-
-    QObject::connect(button, &QPushButton::clicked, [=]() {
-        auto REGION = execAndGet("slurp -f \"%o %x %y %w %h\"");
-        REGION      = REGION.substr(0, REGION.length());
-
-        // now, get the screen
-        QScreen* pScreen = nullptr;
-        if (REGION.find_first_of(' ') == std::string::npos) {
-            std::cout << "error1\n";
-            pickerPtr->quit();
-            return 1;
-        }
-        const auto SCREEN_NAME = REGION.substr(0, REGION.find_first_of(' '));
-
-        for (auto& screen : SCREENS) {
-            if (screen->name().toStdString() == SCREEN_NAME) {
-                pScreen = screen;
-                break;
-            }
-        }
-
-        if (!pScreen) {
-            std::cout << "error2\n";
-            pickerPtr->quit();
-            return 1;
-        }
-
-        // get all the coords
-        try {
-            REGION       = REGION.substr(REGION.find_first_of(' ') + 1);
-            const auto X = std::stoi(REGION.substr(0, REGION.find_first_of(' ')));
-            REGION       = REGION.substr(REGION.find_first_of(' ') + 1);
-            const auto Y = std::stoi(REGION.substr(0, REGION.find_first_of(' ')));
-            REGION       = REGION.substr(REGION.find_first_of(' ') + 1);
-            const auto W = std::stoi(REGION.substr(0, REGION.find_first_of(' ')));
-            REGION       = REGION.substr(REGION.find_first_of(' ') + 1);
-            const auto H = std::stoi(REGION);
-
-            std::cout << "[SELECTION]";
-            std::cout << (ALLOWTOKENBUTTON->isChecked() ? "r" : "");
-            std::cout << "/";
-
-            std::cout << "region:" << SCREEN_NAME << "@" << X - pScreen->geometry().x() << "," << Y - pScreen->geometry().y() << "," << W << "," << H << "\n";
-
-            settings->setValue("width", mainPickerPtr->width());
-            settings->setValue("height", mainPickerPtr->height());
-            settings->sync();
-
-            pickerPtr->quit();
-            return 0;
-        } catch (...) {
-            std::cout << "error3\n";
-            pickerPtr->quit();
-            return 1;
-        }
-
-        std::cout << "error4\n";
-        pickerPtr->quit();
-        return 1;
-    });
-
-    w.show();
-    return picker.exec();
+    window->close();
 }
